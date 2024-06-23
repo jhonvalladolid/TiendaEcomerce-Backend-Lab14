d23afd1db16ee4377dd53e8e085ab029b0d5f57b
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-06-22 22:02:39 -0500

    Serializer mejorado para el registro de datos

--- moda_ecommerce/serializers.py
+++ moda_ecommerce/serializers.py
@@ -8,11 +8,12 @@ class CategoriaSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductoSerializer(serializers.ModelSerializer):
-    categoria = serializers.StringRelatedField()
-    
+    categoria = CategoriaSerializer(read_only=True)  # Anidamos el serializador de Categoría para mostrar todos sus detalles
+    categoria_id = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), source='categoria', write_only=True)
+
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'categoria', 'imagen']
+        fields = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'categoria', 'categoria_id', 'imagen']
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,9 +21,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = ['id', 'nombres', 'apellidos', 'dni', 'telefono', 'direccion', 'email', 'fecha_nacimiento', 'fecha_publicacion']
 
 class VentaSerializer(serializers.ModelSerializer):
-    cliente = ClienteSerializer()
-    producto = ProductoSerializer()
+    cliente = ClienteSerializer(read_only=True)  # Anidamos el serializador de Cliente para mostrar todos sus detalles
+    cliente_id = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all(), source='cliente', write_only=True)
+    producto = ProductoSerializer(read_only=True)  # Anidamos el serializador de Producto para mostrar todos sus detalles
+    producto_id = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all(), source='producto', write_only=True)
 
     class Meta:
         model = Venta
-        fields = ['id', 'cliente', 'producto', 'cantidad', 'precio_total', 'fecha_venta']
+        fields = ['id', 'cliente', 'cliente_id', 'producto', 'producto_id', 'cantidad', 'precio_total', 'fecha_venta']
